81b75640b63afbe629d2e74d9478ed265c2682c1
Chinmay <sahuchinmay27@gmail.com>
2022-08-17 08:10:05 +0530

    first commit django-web proejct

---
+++ mysite/home/views.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from re import TEMPLATE
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates"),'
+)
+
+def index(request):
+    today = datetime.datetime.now().date()
+    return render(request, "index.html", {"today": today})
+
